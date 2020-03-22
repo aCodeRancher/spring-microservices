13fde9827f3c6d2aaa87c976bd515ee60183d38f
helen <hhma@alumni.cpp.edu>
2020-03-22 16:52:51 -0400

    add different profiles for different locales

--- 02.restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/RestfulWebServicesApplication.java
+++ 02.restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/RestfulWebServicesApplication.java
@@ -5,6 +5,7 @@ import java.util.Locale;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
 
@@ -16,9 +17,26 @@ public class RestfulWebServicesApplication {
 	}
 	
 	@Bean
+	@Profile("en")
 	public LocaleResolver localeResolver() {
 		AcceptHeaderLocaleResolver localeResolver = new AcceptHeaderLocaleResolver();
 		localeResolver.setDefaultLocale(Locale.US);
+		return localeResolver;
+	}
+
+	@Bean
+	@Profile("fr")
+	public LocaleResolver localeResolver_fr() {
+		AcceptHeaderLocaleResolver localeResolver = new AcceptHeaderLocaleResolver();
+		localeResolver.setDefaultLocale(Locale.FRENCH);
+		return localeResolver;
+	}
+
+	@Bean
+	@Profile("nl")
+	public LocaleResolver localeResolver_nl() {
+		AcceptHeaderLocaleResolver localeResolver = new AcceptHeaderLocaleResolver();
+		localeResolver.setDefaultLocale(Locale.GERMAN);
 		return localeResolver;
 	}
 }
