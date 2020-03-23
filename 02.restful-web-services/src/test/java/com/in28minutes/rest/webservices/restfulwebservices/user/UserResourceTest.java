bd5a8946cbacdf05b0c48f20530b0ab2ec9a32b9
helen <hhma@alumni.cpp.edu>
2020-03-23 15:38:03 -0400

    WebMvcTest for user resource controller

--- 02.restful-web-services/src/test/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserResourceTest.java
+++ 02.restful-web-services/src/test/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserResourceTest.java
@@ -1,9 +1,15 @@
 package com.in28minutes.rest.webservices.restfulwebservices.user;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.hateoas.Resource;
+import org.springframework.http.ResponseEntity;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -24,10 +30,17 @@ class UserResourceTest {
     @MockBean
     UserDaoService service;
 
+    User user ;
+
+    @BeforeEach
+    public void setUp(){
+          user = new User(1,"Adam", new Date());
+    }
+
     @Test
-    void retrieveAllUsers() {
-          List<User> allUsers = new ArrayList<User>();
-          allUsers.add(new User(1, "Adam", new Date()));
+    public  void retrieveAllUsers() {
+          List<User> allUsers = new ArrayList<>();
+          allUsers.add(user);
           when(controller.retrieveAllUsers()).thenReturn(allUsers);
           List<User> returnUsers = controller.retrieveAllUsers();
           assertTrue(returnUsers.size()==1);
@@ -35,4 +48,45 @@ class UserResourceTest {
     }
 
 
+    @Test
+    public void retrieveUser(){
+        when(service.findOne(1)).thenReturn(user);
+        Resource<User> resourceUser =  controller.retrieveUser(1);
+        assertTrue(resourceUser!=null);
+        assertTrue(resourceUser.getLink("all-users") != null);
+        verify(service,times(1)).findOne(1);
+    }
+
+    @Test
+    public void retrieveNonExistingUser(){
+        user = null;
+        when(service.findOne(2)).thenReturn(user);
+        assertThrows(UserNotFoundException.class, ()-> controller.retrieveUser(2) );
+        verify(service,times(1)).findOne(2);
+     }
+
+     @Test
+     public void deleteUser(){
+        when(service.deleteById(1)).thenReturn(user);
+        controller.deleteUser(1);
+        verify(service,times(1)).deleteById(1);
+     }
+
+     @Test
+     public void deleteNonExistingUser(){
+        user = null;
+        when(service.deleteById(1)).thenReturn(user);
+        assertThrows(UserNotFoundException.class, ()-> controller.deleteUser(1));
+     }
+
+     @Test
+     public void createUser(){
+        //since the user is a request body, we need to create this mock servlet request.
+         //reference : https://howtodoinjava.com/spring-boot2/testing/rest-controller-unit-test-example/
+         MockHttpServletRequest request = new MockHttpServletRequest();
+         RequestContextHolder.setRequestAttributes(new ServletRequestAttributes(request));
+         when(service.save(user)).thenReturn(user);
+         ResponseEntity<Object> response = controller.createUser(user);
+         assertTrue(response.getHeaders().getLocation().getPath().equals("/1"));
+    }
 }
