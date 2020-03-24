d513f5346b7a40dfc221d2d39764c290af982253
helen <hhma@alumni.cpp.edu>
2020-03-24 15:30:06 -0400

    add two constructors to Post

--- 02.restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/Post.java
+++ 02.restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/Post.java
@@ -1,10 +1,6 @@
 package com.in28minutes.rest.webservices.restfulwebservices.user;
 
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
@@ -19,7 +15,15 @@ public class Post {
 	@ManyToOne(fetch=FetchType.LAZY)
 	@JsonIgnore
 	private User user;
-	
+
+	public Post() {}
+
+	public Post (Integer id, String description, User user){
+		this.id = id;
+		this.description = description;
+		this.user = user;
+	}
+
 	public Integer getId() {
 		return id;
 	}
