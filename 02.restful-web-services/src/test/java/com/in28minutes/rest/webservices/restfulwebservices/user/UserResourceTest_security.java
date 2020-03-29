7f31418aec104f07a9808abc18fd52b1b5c46a8c
helen <hhma@alumni.cpp.edu>
2020-03-29 19:18:59 -0400

    add basic authentication test

---
+++ 02.restful-web-services/src/test/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserResourceTest_security.java
@@ -0,0 +1,37 @@
+package com.in28minutes.rest.webservices.restfulwebservices.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class UserResourceTest_security {
+
+   @LocalServerPort
+   int port;
+
+   @Test
+    public void retrieveAllUsers() {
+        TestRestTemplate testRestTemplate = new TestRestTemplate("username", "password");
+        ResponseEntity<List> response = testRestTemplate.getForEntity("http://localhost:"+port+"/users", List.class);
+        assertTrue(response.getStatusCodeValue()==200);
+        List<User> result = response.getBody();
+        assertTrue(result.size()==3);
+    }
+
+    @Test
+    //Since the credentials are wrong, it will navigate to the sign in page.
+    public void retrieveAllUser_notAuthenticated(){
+        TestRestTemplate testRestTemplate = new TestRestTemplate("noUser", "noPassword");
+        ResponseEntity<String> response = testRestTemplate.getForEntity("http://localhost:"+port+"/users",String.class);
+        assertTrue(response.getStatusCodeValue()==200);
+        assertTrue(response.toString().contains("Please sign in"));
+    }
+
+}
