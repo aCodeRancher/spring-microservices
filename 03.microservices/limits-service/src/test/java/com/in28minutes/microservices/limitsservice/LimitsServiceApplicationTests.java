a7a5b26f2a91777c6686e0a5e79bd2553db626f0
helen <hhma@alumni.cpp.edu>
2020-03-31 16:18:45 -0400

    add LimitServiceApplicationTest

--- 03.microservices/limits-service/src/test/java/com/in28minutes/microservices/limitsservice/LimitsServiceApplicationTests.java
+++ 03.microservices/limits-service/src/test/java/com/in28minutes/microservices/limitsservice/LimitsServiceApplicationTests.java
@@ -1,16 +1,26 @@
 package com.in28minutes.microservices.limitsservice;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.junit.jupiter.api.Test;
 
-@RunWith(SpringRunner.class)
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+
 @SpringBootTest
 public class LimitsServiceApplicationTests {
 
+	int port = 8080;
+
 	@Test
-	public void contextLoads() {
+	public void retrieveLimits(){
+		String expected = "{\"maximum\":2222,\"minimum\":2}";
+		TestRestTemplate testRestTemplate = new TestRestTemplate();
+		ResponseEntity<String> response = testRestTemplate.getForEntity("http://localhost:"+port+"/limits", String.class);
+		assertTrue(response.getBody().contains(expected));
 	}
 
 }
