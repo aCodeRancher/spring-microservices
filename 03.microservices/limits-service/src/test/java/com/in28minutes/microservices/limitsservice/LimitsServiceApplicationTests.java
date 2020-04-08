8978d337670b490d0e18e7466db12e0d5f67a72f
helen <hhma@alumni.cpp.edu>
2020-04-08 18:29:35 -0400

    add fault tolerant test

--- 03.microservices/limits-service/src/test/java/com/in28minutes/microservices/limitsservice/LimitsServiceApplicationTests.java
+++ 03.microservices/limits-service/src/test/java/com/in28minutes/microservices/limitsservice/LimitsServiceApplicationTests.java
@@ -1,25 +1,30 @@
 package com.in28minutes.microservices.limitsservice;
 
 import org.junit.jupiter.api.Test;
-
-
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.ResponseEntity;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-
-
-@SpringBootTest
 public class LimitsServiceApplicationTests {
 
-	int port = 8080;
+	int port =8080 ;
 
 	@Test
+	//run  SpringCloudConfigServerApplication first, then run LimitsServiceApplication
+	//next, run this test
 	public void retrieveLimits(){
-		String expected = "{\"maximum\":2222,\"minimum\":2}";
+		String expected = "{\"maximum\":9999,\"minimum\":103}";
 		TestRestTemplate testRestTemplate = new TestRestTemplate();
 		ResponseEntity<String> response = testRestTemplate.getForEntity("http://localhost:"+port+"/limits", String.class);
+		assertTrue(response.getBody().contains(expected));
+	}
+
+	@Test
+	//run the LimitServiceApplication and run this test, don't run the SpringCloudConfigServerApplication
+	public void faultTolerateTest(){
+		String expected = "{\"maximum\":999,\"minimum\":9}";
+		TestRestTemplate testRestTemplate = new TestRestTemplate();
+		ResponseEntity<String> response = testRestTemplate.getForEntity("http://localhost:"+port+"/fault-tolerance-example", String.class);
 		assertTrue(response.getBody().contains(expected));
 	}
 
