b53e353f91ad84194d525376e349740f7d909060
helen <hhma@alumni.cpp.edu>
2020-07-06 20:29:12 -0400

    add this class to help a student solving his problem

---
+++ 02.restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserResponse.java
@@ -0,0 +1,13 @@
+package com.in28minutes.rest.webservices.restfulwebservices.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class UserResponse {
+
+    private String message;
+
+
+}
