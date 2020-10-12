9a206030254c49d4a551c31ed419476627af7071
helen <hhma@alumni.cpp.edu>
2020-10-11 21:31:21 -0400

    put the ExchangeValueRepository back

--- 03.microservices/currency-exchange-service/src/main/java/com/in28minutes/microservices/currencyexchangeservice/CurrencyExchangeController.java
+++ 03.microservices/currency-exchange-service/src/main/java/com/in28minutes/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -1,6 +1,6 @@
 package com.in28minutes.microservices.currencyexchangeservice;
 
-import com.in28minutes.microservices.currencyexchangeservice.repository.ExchangeValueRepository;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
