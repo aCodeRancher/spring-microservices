163fb5c3c8945c558a792e41a543ffe57b728507
helen <hhma@alumni.cpp.edu>
2020-11-13 16:37:58 -0500

    user EnableSwagger2

--- 02.restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/SwaggerConfig.java
+++ 02.restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/SwaggerConfig.java
@@ -11,10 +11,12 @@ import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
 
 @Configuration
-@EnableSwagger2WebMvc
+//@EnableSwagger2WebMvc
+@EnableSwagger2
 public class SwaggerConfig {
 
 	public static final Contact DEFAULT_CONTACT = new Contact(
