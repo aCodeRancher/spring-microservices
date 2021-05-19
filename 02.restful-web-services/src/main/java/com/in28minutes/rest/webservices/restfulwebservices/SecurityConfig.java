db5a48261f0e1b9f401e50374e31513dd382c885
helen <hhma@alumni.cpp.edu>
2021-05-18 21:01:44 -0400

    permit all roles to /h2-console/

--- 02.restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/SecurityConfig.java
+++ 02.restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/SecurityConfig.java
@@ -5,13 +5,13 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
-public class SecurityConfig  extends WebSecurityConfigurerAdapter {
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/h2-console").permitAll()
+                .antMatchers("/h2-console/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
                  .httpBasic();
